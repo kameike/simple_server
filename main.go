5dc1c87acae3f4cdac4c85d6aef2307a167c247c
kameike <kameike@users.noreply.github.com>
2018-03-15 00:52:52 +0900

    update

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	//"github.com/go-redis/redis"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"go-redis/redis"
 	"net/http"
 	"os"
-	"strconv"
 )
+
+var redis_host = os.Getenv("REDIS_HOST")
+var redis_port = os.Getenv("REDIS_PORT")
+var db_host = os.Getenv("DB_HOST")
+var db_port = os.Getenv("DB_PORT")
+var time = 0
 
 func main() {
 	// Echo instance
@@ -24,10 +31,16 @@ func main() {
 	e.Logger.Fatal(e.Start(":3000"))
 }
 
-var time = 0
-
 // Handler
 func hello(c echo.Context) error {
 	time = time + 1
-	return c.String(http.StatusOK, "Hello, World!"+strconv.Itoa(time))
+	var buf = bytes.NewBufferString("[status]\n")
+	fmt.Fprintln(buf, "Hello, World!")
+	fmt.Fprintf(buf, "TotalInMemoryReloadCount => %d\n", time)
+	fmt.Fprintf(buf, "redis_host => %s\n", redis_host)
+	fmt.Fprintf(buf, "redis_port => %s\n", redis_port)
+	fmt.Fprintf(buf, "db_host => %s\n", db_host)
+	fmt.Fprintf(buf, "db_port => %s\n", db_port)
+
+	return c.String(http.StatusOK, buf.String())
 }
